5e2c7ebe3848a2b15955b176a58a5afa78c897a7
Ty Tia <tytia77@gmail.com>
2017-05-09 15:28:56 +1000

    style changes

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ var app = angular.module('WikiViewer', []);
 
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 
-    $('.search-icon').on("click", function(ev){
-        if(ev.target.className === 'cross'){
-            $('.search-input').removeClass('expand-search');
-            $('form').removeClass('open');
-            $('.search-handle').removeClass('collapse-search-handle');
-            $('.search-input').attr('disabled', '');
-        } else{
-            $('.search-input').addClass('expand-search');
-            $('form').addClass('open');
-            $('.search-handle').addClass('collapse-search-handle');
-            $('.search-input').attr('disabled', false);
-        }
+    $('.search-input').on("click", function(){
+        $('.search-input').addClass('expand-search');
+        $('form').addClass('open');
+        $('.search-icon').addClass('open');
+    });
+
+    $('.cross').on('click', function(){
+        $('.search-input').removeClass('expand-search');
+        $('form').removeClass('open');
+        $('.search-icon').removeClass('open');
     });
 
     $scope.search = function(){
-
         $scope.results = [];
         var container = document.querySelector('.container-fluid');
         var page = 'https://en.wikipedia.org/?curid=';
@@ -25,8 +22,12 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
         // var api = 'https://en.wikipedia.org/w/api.php?format=json&action='
         //     +'query&generator=search&gsrnamespace=0&gsrlimit=10&prop='
         //     +'pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-        var api = 'https://en.wikipedia.org/w/api.php?format=json&action='
-            +'query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=';
+        // var api = 'https://en.wikipedia.org/w/api.php?format=json&action='
+        //     +'query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=';
+        var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json';
+            api+= '&prop=extracts&continue=gsroffset%7C%7C&generator=search';
+            api+= '&exsentences=1&exlimit=15&exintro=1&explaintext=1&gsrlimit=15';
+            api+= '&gsroffset=15&origin=*&gsrsearch=';
 
             $http.get(api +search).then(function(data){
                 console.log(data);
@@ -34,7 +35,8 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
                     angular.forEach(data.data.query.pages, function(item){
                         $scope.results.push({
                             title: item.title,
-                            url: page +item.pageid
+                            url: page +item.pageid,
+                            info: item.extract
                         });
                     });
 
