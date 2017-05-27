2cafe17b989b1f8a238ab5a13779f866509d5c3b
Ty Tia <TyTia77@gmail.com>
2017-05-27 14:40:50 +1000

    fix api

--- script.js
+++ script.js
@@ -20,18 +20,12 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
         var container = document.querySelector('.container-fluid');
         var page = 'https://en.wikipedia.org/?curid=';
         var search = $('input').val();
-        // var api = 'https://en.wikipedia.org/w/api.php?format=json&action='
-        //     +'query&generator=search&gsrnamespace=0&gsrlimit=10&prop='
-        //     +'pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-        // var api = 'https://en.wikipedia.org/w/api.php?format=json&action='
-        //     +'query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=';
+        var cors = 'https://cors-anywhere.herokuapp.com/';
         var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json';
-            api+= '&prop=extracts&continue=gsroffset%7C%7C&generator=search';
-            api+= '&exsentences=1&exlimit=15&exintro=1&explaintext=1&gsrlimit=15';
-            api+= '&gsroffset=15&origin=*&gsrsearch=';
+            api += '&prop=extracts&generator=search&exsentences=1&exlimit=10&exintro=1';
+            api += '&explaintext=1&gsrnamespace=0&gsrlimit=10&gsrsearch=';
 
-            $http.get(api +search).then(function(data){
-                console.log(data);
+            $http.get(cors +api +search).then(function(data){
                 if(data.data.query){
                     angular.forEach(data.data.query.pages, function(item){
                         $scope.results.push({
